1c4b1bfee78a0345f9b824e50a0b11c5e56c526d
amineis <amineidrissis12@gmail.com>
2026-01-21 01:10:28 +0100

    first commit

---
+++ src/main/java/dev/amineis/gameadmin/GameadminApplication.java
@@ -0,0 +1,13 @@
+package dev.amineis.gameadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameadminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GameadminApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/amineis/gameadmin/GameadminApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.amineis.gameadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameadminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
