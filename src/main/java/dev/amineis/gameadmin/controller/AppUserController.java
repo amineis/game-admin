e7c4fb8ac967f3bc9603ef1f6fff18e8b5a0cc4e
amineis <amineidrissis12@gmail.com>
2026-02-02 02:20:04 +0100

    Setup keycloak client with the API

---
+++ src/main/java/dev/amineis/gameadmin/controller/AppUserController.java
@@ -0,0 +1,31 @@
+package dev.amineis.gameadmin.controller;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/app-users")
+@RequiredArgsConstructor
+public class AppUserController {
+
+    @GetMapping
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<String> getAllAppUsers() {
+        return ResponseEntity.ok("hello");
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<String> getCurrentUser(@AuthenticationPrincipal Jwt jwt) {
+        String username = jwt.getClaim("preferred_username");
+        String email = jwt.getClaim("email");
+        return ResponseEntity.ok("Hello, " + username + " (" + email + ")");
+    }
+}
+
